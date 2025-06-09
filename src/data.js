0eff28c2197e1e2247c53198a30e81f6038982ed
AnnaShell <anna.tityushina@yandex.ru>
2025-06-09 12:41:28 +0300

    content upd

--- src/data.js
+++
@@ -1,9 +0,0 @@
-export const lessons = [
-  {
-    id: 1,
-    title: "Фонетика: Основы",
-    content: "Изучаем базовые звуки английского языка...",
-    code: "function pronounce() {\n  return 'Hello';\n}"
-  },
-  // ...другие уроки
-];
