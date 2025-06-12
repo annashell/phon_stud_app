dbab989446cf242f682cf785a98aa660d95f4a0e
annashell <annaolegovna3@rambler.ru>
2025-06-12 11:31:02 +0300

    micro fix

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -4,16 +4,23 @@ import { chapters } from '../content';
 export default function Contents() {
   return (
     <div className="contents-page">
-      <h2>Содержание учебника</h2>
-      <ul className="contents-list">
-        {chapters.map(chapter => (
-          <li key={chapter.id}>
+      <h3>Содержание</h3>
+      {chapters.map(chapter => {
+        const level = chapter.id.split('-').length - 1;
+        return (
+          <div 
+            key={chapter.id} 
+            style={{ 
+              paddingLeft: `${level * 40}px`,
+              margin: '5px 0'
+            }}
+          >
             <Link to={`/chapter/${chapter.id}`}>
               {chapter.title}
             </Link>
-          </li>
-        ))}
-      </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
