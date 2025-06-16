4e10d7e2f7df4335e0b4d5fe018e0d800ca8d862
AnnaShell <anna.tityushina@yandex.ru>
2025-06-16 11:20:20 +0300

    add app title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import ChapterView from './components/ChapterView';
@@ -7,6 +8,12 @@ import Home from './pages/Home';
 import Contents from './components/Contents';
 
 export default function App() {
+  const title = "История фонетических исследований";
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
     <div className="app">
       <Navbar />
